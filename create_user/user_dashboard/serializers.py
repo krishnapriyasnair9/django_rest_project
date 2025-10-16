3801a0511305d7ddf3f836202b56b1f4da7a7c9b
Krishnapriya S Nair <krishnapriyasnair9@gmail.com>
2025-10-16 12:34:41 +0530

    Updated RegisterView and serializers

--- create_user/user_dashboard/serializers.py
+++ create_user/user_dashboard/serializers.py
@@ -13,10 +13,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         write_only=True, required=True, validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
+    phone_number=serializers.CharField(required=False)
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name', 'phone_number')
+        fields = ('username', 'email', 'password', 'password2', 'phone_number')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -27,8 +28,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = CustomUser.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data.get('first_name', ''),
-            last_name=validated_data.get('last_name', ''),
+            
             phone_number=validated_data.get('phone_number', '')
         )
         user.set_password(validated_data['password'])
@@ -39,4 +39,4 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'phone_number')
+        fields = ('id', 'username', 'email',  'phone_number')
--- create_user/user_dashboard/views.py
+++ create_user/user_dashboard/views.py
@@ -2,15 +2,24 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser
 from .serializers import RegisterSerializer, UserSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     
-
 class ProfileView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
 
     def get_object(self):
         return self.request.user
+    def destroy(self, request, *args, **kwargs):
+        user = self.get_object()
+        username = user.username
+        user.delete()
+        return Response(
+            {"message": f"User '{username}' deleted successfully."},
+            status=status.HTTP_200_OK
+        )
+
